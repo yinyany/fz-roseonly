3eb7324390ad092731b730de496aca549a328134
yinyan <15383598549@163.com>
2018-03-15 18:55:32 +0800

    前台登陆注册(重置密码未完成)

---
+++ fz-roseonly/app/Http/Controllers/Authindex/PasswordController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Authindex;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+class PasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('authindex.login_edit');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function postemail()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function getreset(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function postreset($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- fz-roseonly/resources/views/authindex/login_register.blade.php
+++ fz-roseonly/resources/views/authindex/login_register.blade.php
@@ -3,13 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>注册</title>
-    <link rel="stylesheet" type="text/css" href="../css/register.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('static/index/css/login_register.css') }}">
 </head>
 <body>
 	<!-- ++++++++++ logo +++++++++++ -->
     <div id="logo">
         <a href="index.html">
-            <img src="../images/comment/logo.jpg" alt="roseonly官网" title="roseonly官网">
+            <img src="{{ asset('static/index/images/comment/logo.jpg') }}" alt="roseonly官网" title="roseonly官网">
         </a>
     </div>
     <!-- 导航 -->
@@ -19,68 +19,64 @@
     	<!-- 左半边 -->
     	<div class="register_bj">
     		<form action="" >
-                <div class="register_phote" style="margin-top:20px;">
+            <!--     <div class="register_phote" style="margin-top:20px;">
                     <input type="text" placeholder="用户名" name="username"  maxlength="30" class="register_text ">
                    
-               </div>  
+               </div>   -->
                  <!-- 手机号错误提示信息 -->
-                <div class="reminder_validate" >
-                    <img src="../images/login/warning.png" alt="">
+                <!-- <div class="reminder_validate" >
+                    <img src="{{ asset('static/index/images/login/warning.png') }}" alt="">
                    请填写正确的用户名
-                </div> 
-                <div class="register_phote">
+                </div>  -->
+                <div class="register_phote"style="margin-top:30px;">
                     <input type="text" placeholder="邮箱" name="username"  maxlength="30" class="register_text ">
                    
                </div>  
                  <!-- 手机号错误提示信息 -->
                 <div class="reminder_validate" >
-                    <img src="../images/login/warning.png" alt="">
+                    <img src="{{ asset('static/index/images/login/warning.png') }}" alt="">
                    请填写正确的邮箱地址
                 </div>    
                 
                 <!-- 验证码 -->
-                <div class="register_yzmpas" >
+               <!--  <div class="register_yzmpas" >
                     <input type="text" name="rand" placeholder="图片验证码" maxlength="4" class="register_yzm" id="rand_yzm">
                     <div class="register_yzmimg">
-                        <img  src="../images/login/3.jpg" width="100px" height="47px" alt="验证码" title="验证码" onclick="javascript:refreshRand();">
+                        <img  src="{{ asset('static/index/images/login/3.jpg') }}" width="100px" height="47px" alt="验证码" title="验证码" onclick="javascript:refreshRand();">
                         
                     </div>
         
-                </div>
+                </div> -->
                 <!-- 验证码提示信息 -->
-                <div class="reminder_validate" >
-                    <img src="../images/login/warning.png" alt="">
+                <!-- <div class="reminder_validate" >
+                    <img src="{{ asset('static/index/images/login/warning.png') }}" alt="">
                      请输入4位图片验证码                     
-                </div>  
+                </div>   -->
 
              
                <div class="register_phote">
-                    <input input type="password" name="password" placeholder="输入密码"  maxlength="30" class="register_text " >
+                    <input input type="password" name="password" placeholder="重置密码"  maxlength="30" class="register_text " >
                    
                </div>
                 <!-- 验证码提示信息 -->
                 <div class="reminder_validate" >
-                    <img src="../images/login/warning.png" alt="">
+                    <img src="{{ asset('static/index/images/login/warning.png') }}" alt="">
                      请输入正确的密码                    
                 </div>  
 
                <div class="register_phote">
-                    <input input type="password" name="password" placeholder="确认密码"  maxlength="30" class="register_text " >
+                    <input input type="password" name="password" placeholder="确认重置密码"  maxlength="30" class="register_text " >
                    
                </div>
                 <!-- 验证码提示信息 -->
                 <div class="reminder_validate" >
-                    <img src="../images/login/warning.png" alt="">
+                    <img src="{{ asset('static/index/images/login/warning.png') }}" alt="">
                      请输入正确的密码                      
                 </div>  
-
-                <!-- <div class="signin_font">
-                    <a href="javascript:;">创建账号</a>
-                </div> -->
                 
                 <div class="signin_regist">
-                    <input type="submit" value="创建账号">
-                    <!-- <a href="javascript:;"></a> -->
+                    <input type="submit" value="找回密码">
+                    
                 </div>
             </form>
     		
